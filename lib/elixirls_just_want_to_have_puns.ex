e883f35a5f0aa38f382b9c5d5e16ca19532bde1a
Ian C. Anderson <ian@iancanderson.com>
2015-10-17 09:49:51 -0400

    Rename - 'make' was overloaded

--- lib/elixirls_just_want_to_have_puns.ex
+++ lib/elixirls_just_want_to_have_puns.ex
@@ -32,7 +32,7 @@ defmodule ElixirlsJustWantToHavePuns do
 
     rhymebrain_results
     |> map fn(rhymebrain_result) ->
-      PunMaker.spawn_make(self, word, rhymebrain_result)
+      PunFinder.spawn_from_rhyme(self, word, rhymebrain_result)
     end
 
     receive_puns(length(rhymebrain_results), [])
@@ -56,18 +56,18 @@ defmodule ElixirlsJustWantToHavePuns do
   end
 end
 
-defmodule PunMaker do
+defmodule PunFinder do
   import Enum
 
-  def spawn_make(pid, original_word, rhymebrain_result) do
-    spawn __MODULE__, :send_make, [pid, original_word, rhymebrain_result]
+  def spawn_from_rhyme(pid, original_word, rhymebrain_result) do
+    spawn __MODULE__, :send_from_rhyme, [pid, original_word, rhymebrain_result]
   end
 
-  def send_make(pid, original_word, rhymebrain_result) do
-    send pid, make(original_word, rhymebrain_result)
+  def send_from_rhyme(pid, original_word, rhymebrain_result) do
+    send pid, from_rhyme(original_word, rhymebrain_result)
   end
 
-  def make(original_word, rhymebrain_result) do
+  def from_rhyme(original_word, rhymebrain_result) do
     Phrases.with_word(rhymebrain_result.word)
     |> map(&(Pun.make(&1, original_word, rhymebrain_result)))
   end
